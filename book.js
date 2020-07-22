a581acd817cf4b263178dc763b533ddc2bf109ea
Lee Doughty <leedoughtydesign@gmail.com>
2020-07-22 15:40:56 +0100

    Map list of books to class

---
+++ book.js
@@ -0,0 +1,11 @@
+class Book {
+  constructor(id, title, author, isbn, barcode) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.barcode = barcode;
+  }
+}
+
+module.exports = { Book };
--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const db = require("./databaseConnection");
+const { Book } = require("./book");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/", (request, response) => {
+  response.send("Hello World");
 });
 
-app.get("/books", (req, res) => {
-  db.any("SELECT * FROM books").then((data) => res.send(data));
+app.get("/books", (request, response) => {
+  db.any("SELECT * FROM books")
+    .then((data) => {
+      const listBooks = data.map((book) => {
+        return new Book(
+          book.id,
+          book.title,
+          book.author,
+          book.isbn,
+          book.barcode
+        );
+      });
+      console.log(listBooks);
+      response.send(listBooks);
+    })
+    .catch((error) => {
+      console.log("error", error);
+    });
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
